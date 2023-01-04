3a7fdb836adec4903371285ef5be6a01719fc4c3
tolanie <florenceabiodun2000@yahoo.com>
2023-01-04 23:57:24 +0100

    Slick slider

--- src/components/main/SlideCard.jsx
+++ src/components/main/SlideCard.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
 // import Sdata from "./Sdata"
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const SlideCard = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    appendDots: (dots) => {
+      return <ul style={{margin:"0px"}}>{dots} </ul>
+    }
+  };
  
   const Sdata = [
 
@@ -35,6 +49,8 @@ const SlideCard = () => {
 ]
         return (
               <>
+                    <Slider {...settings}>
+
                {Sdata.map((value, index) => {
                  return(
                    <div className="box d_flex top" key={index}>
@@ -50,6 +66,7 @@ const SlideCard = () => {
                    </div>
               )
     })}
+      </Slider>
     </>
   )
 }
